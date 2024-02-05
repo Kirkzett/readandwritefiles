82add458bf1e5af4556e65cb8414cb445637c68c
Kirk <kirk_zetterstrom1@baylor.edu>
2024-02-05 13:06:49 -0600

    Update

--- employee_pay.py
+++ employee_pay.py
@@ -4,7 +4,6 @@ employee = open('employee_data.csv','r')
 
 csv_file = csv.reader(employee)
 
-print("Name, Age, Salary, Hours, Productivity, Team, Bonus" + "\n")
               
               
 
@@ -12,13 +11,12 @@ next(csv_file)
 num = 0
 #write new lines
 for rec in csv_file:
-    print(f"Name: {rec[1]}")
-    print(f"Salary: $ {rec[2]:2d")
+    salary = int(rec[3])
     bonus = float(rec[7])
-    salary = float(rec[3])
-    bonus_calc = salary + (salary * bonus)
-    print(f"{rec[1]}, {rec[2]}, {rec[3]}, {rec[4]}, {rec[5]}, {rec[6]}, {bonus_calc} \n")
+    total_bonus = (salary * bonus)
+    total = salary + total_bonus
+    print(f" Name:   {rec[1]}")
+    print(f" Salary: ${salary}")
+    print(f" Bonus:  ${total_bonus}")
+    print(f" Pay:    ${total}")
     input()
-    num += 1
-
-print(f"Total Cusotmers: {num}")
